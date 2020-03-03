c95c05d04070c895391ed62237da230423186da9
Mitzi Estrada <m_estrada17@live.com>
2020-03-03 05:02:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,32 @@
 class Dog
   
+  @@all = { }
   
+  def initialize
+    @name = name
+    @@all << self
+  end
+  
+  def all
+    @@all
+  end
+  
+  def print_all(name)
+    @@all.each do |individual|
+      puts #{name}
+  end
+  
+  def save(name)
+    @@all << self
+  end
+  
+  def clear_all
+    
+  end
+  
+  
+    
+    
   
   
 end
